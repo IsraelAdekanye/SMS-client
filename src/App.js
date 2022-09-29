16d951aa9643188a43c89b2c1a39b0080c043099
IsraelMacBook <israeladedigba@yahoo.com>
2022-09-29 11:45:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import {React, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import StudentRegistration from './components/StudentRegistration';
+
+
 
 function App() {
+
+  const [data,setData]=useState([]);
+
+  let tade = data.map((e, index)=>{
+    return (
+      
+      <div key={index} className="table-content">
+			<div className="table-row">		
+				<div className="table-data">{e.id}</div>
+				<div className="table-data">{e.firstname}</div>
+				<div className="table-data">{e.lastname}</div>
+				<div className="table-data"> <button>Pay</button> </div>
+				<div className="table-data"><button>View Results</button></div>
+			</div>
+      </div>
+    )
+   })
+
+  let baseUrl = "http://localhost:3030/more/eniola"
+
+  // const getInfo = (url) => {
+  //   fetch(url)
+  //   .then(res=>{
+  //     //res.json()
+  //     console.log(res.body);
+  //   }).then(data=>{
+  //     result.push(data);
+  //   })
+  // }
+
+  const getInfo=()=>{
+    axios.get(baseUrl)
+        .then(res=>{
+            setData([res.data]);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+}
+    
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {tade}
+      <button onClick={getInfo} >Get</button>
+      <StudentRegistration></StudentRegistration>
+     
     </div>
   );
 }
